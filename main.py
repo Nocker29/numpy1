c5df456fe4c397170ad5b9467fa59b3f61eb5dcf
Nocker29 <49701710+Nocker29@users.noreply.github.com>
2021-04-25 19:11:50 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+#zad2
+# a=np.array([1,2,3,4],dtype="float")
+# print(a.dtype)
+# b=a.astype('int64')
+# print(b.dtype)
+
+#zad3
+# def macierz(n):
+#     tab=np.arange(1,n*n+1,1)
+#     tab=tab.reshape((n,n))
+#     return tab
+
+# tab=macierz(6)
+# print(tab)
+
+#zad5
+# def macierz_diag(x):
+#     mat_diag=np.diag([a for a in range(x,0,-1)])
+#     return mat_diag
+#
+# tab=macierz_diag(5)
+# print(tab)
